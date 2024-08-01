2a63dcfe046b1f8188d3ef41bf93de61144a59c6
heondolee <heondo999@inu.ac.kr>
2024-08-01 14:03:12 +0900

    users 넣기 완료

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Named import for Link
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // 여기에 로그인 처리 로직 추가
+    console.log('Username:', username, 'Password:', password);
+  };
+
   return (
-    <div>
-      <h1>로그인 페이지</h1>
-      <Button as={Link} to="/long">장기보험 바로가기</Button>
-    </div>
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col xs={12} md={6}>
+          <h2 className="text-center">INSVIL 로그인</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>아이디</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="아이디를 입력하세요."
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword" className="mt-3">
+              <Form.Label>비밀번호</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="비빌번호를 입력하세요."
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="mt-3">
+              Login
+            </Button>
+          </Form>
+          <div className="mt-3">
+            <Link to="/signup">회원가입하기</Link>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
