1ce72cd780b33e5b0ab92aebedb60917c84acfab
heondolee <heondo999@inu.ac.kr>
2024-08-01 14:30:36 +0900

    토큰 없이 로그인 성공

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Login from './components/Login';
 import Long from './components/Long';
 import Car from './components/Car';
+import Home from './components/Home';
 import Normal from './components/Normal';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/long" element={<Long />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/car" element={<Car/>} />
       <Route path="/normal" element={<Normal/>} />
       <Route path="*" element={<Navigate to="/" />} />
