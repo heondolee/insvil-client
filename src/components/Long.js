c6603648eb0c75a6ce5f75e5eb29f1b23aec490f
heondolee <heondo999@inu.ac.kr>
2024-08-04 23:50:39 +0900

    모바일 수정

--- src/components/Long.js
+++ src/components/Long.js
@@ -7,7 +7,6 @@ import styles from './css/Long.module.css'; // 모듈 import
 const API_URL = process.env.REACT_APP_API_URL;
 
 const Long = () => {
-  // 현재 날짜를 가져오는 함수
   const getCurrentDate = () => {
     const today = new Date();
     const year = today.getFullYear();
@@ -17,11 +16,13 @@ const Long = () => {
   };
 
   const [data, setData] = useState([]);
-  const [startDate, setStartDate] = useState(getCurrentDate()); // 기본값을 현재 날짜로 설정
-  const [endDate, setEndDate] = useState(getCurrentDate()); // 기본값을 현재 날짜로 설정
-  const [dateType, setDateType] = useState('contractDate'); // 날짜 유형 상태 추가
-  const [contractStatus, setContractStatus] = useState('statusAll'); // 계약 상태 상태 추가
-  const [isLoading, setIsLoading] = useState(false); // 로딩 상태 추가
+  const [startDate, setStartDate] = useState(getCurrentDate());
+  const [endDate, setEndDate] = useState(getCurrentDate());
+  const [dateType, setDateType] = useState('contractDate');
+  const [contractStatus, setContractStatus] = useState('statusAll');
+  const [contractor, setContractor] = useState('');
+  const [policyNumber, setPolicyNumber] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = useCallback(async () => {
     setIsLoading(true);
@@ -29,23 +30,23 @@ const Long = () => {
       const response = await axios.post(`${API_URL}/long/date-range`, {
         startDate,
         endDate,
-        dateType, // 날짜 유형을 요청에 포함
-        contractStatus // 계약 상태를 요청에 포함
+        dateType,
+        contractStatus,
+        contractor,
+        policyNumber
       });
       setData(response.data.longs);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
     setIsLoading(false);
-  }, [startDate, endDate, dateType, contractStatus]);
+  }, [startDate, endDate, dateType, contractStatus, contractor, policyNumber]);
 
-  // 숫자를 1,000 단위로 구분하는 함수
   const formatNumber = (num) => {
     const formattedNum = new Intl.NumberFormat().format(num * 1000);
     return formattedNum;
   };
 
-  // 회차를 0/120 형식으로 변환하는 함수
   const formatTerm = (totalTerm) => {
     return `0/${totalTerm}`;
   };
@@ -56,31 +57,31 @@ const Long = () => {
       <Container>
         <Form>
           <Row className="align-items-center">
-            <Col xs="auto">
-            <Form.Group controlId="formDateRange">
-              <DropdownButton
-                variant="outline-secondary"
-                title={
-                  "검색일 : " + 
-                  (dateType === 'contractDate' ? '계약일' : 
-                  dateType === 'paymentEndDate' ? '만기일' : 
-                  '개시일')
-                }
-                onSelect={(eventKey) => setDateType(eventKey)}
-              >
-                <Dropdown.Item eventKey="contractDate">계약일</Dropdown.Item>
-                <Dropdown.Item eventKey="paymentStartDate">개시일</Dropdown.Item>
-                <Dropdown.Item eventKey="paymentEndDate">만기일</Dropdown.Item>
-              </DropdownButton>
-            </Form.Group>
-            </Col>
-            <Col xs="auto">
-              <Form.Group controlId="formDateRange">
+            <Col xs={12} md="auto">
+              <Form.Group controlId="formDateType">
+                <Form.Label>검색일 :</Form.Label>
                 <DropdownButton
                   variant="outline-secondary"
                   title={
-                    "계약상태 : " +
-                    (contractStatus === 'statusAll'
+                    dateType === 'contractDate' ? '계약일' : 
+                    dateType === 'paymentEndDate' ? '만기일' : 
+                    '개시일'
+                  }
+                  onSelect={(eventKey) => setDateType(eventKey)}
+                >
+                  <Dropdown.Item eventKey="contractDate">계약일</Dropdown.Item>
+                  <Dropdown.Item eventKey="paymentStartDate">개시일</Dropdown.Item>
+                  <Dropdown.Item eventKey="paymentEndDate">만기일</Dropdown.Item>
+                </DropdownButton>
+              </Form.Group>
+            </Col>
+            <Col xs={12} md="auto">
+              <Form.Group controlId="formContractStatus">
+                <Form.Label>계약상태 :</Form.Label>
+                <DropdownButton
+                  variant="outline-secondary"
+                  title={
+                    contractStatus === 'statusAll'
                       ? '전체'
                       : contractStatus === 'statusMaintain'
                       ? '유지'
@@ -94,7 +95,7 @@ const Long = () => {
                       ? '취소'
                       : contractStatus === 'statusExpire'
                       ? '만기'
-                      : '전체')
+                      : '전체'
                   }
                   onSelect={(eventKey) => setContractStatus(eventKey)}
                 >
@@ -108,14 +109,34 @@ const Long = () => {
                 </DropdownButton>
               </Form.Group>
             </Col>
-            < Col xs="auto">
-              <Form.Group controlId="formDateRange">
-                <Form.Control type="password" placeholder="Password" />
+            <Col xs={12} md="auto">
+              <Form.Group controlId="formContractor">
+                <Form.Label>계약자 :</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder='계약자 이름:'
+                  value={contractor}
+                  onChange={(e) => setContractor(e.target.value)}
+                  className={styles.form_control_custom}
+                />
               </Form.Group>
             </Col>
-            <Col xs="auto">
+            <Col xs={12} md="auto">
+              <Form.Group controlId="formPolicyNumber">
+                <Form.Label>증권번호 :</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder='증권번호:'
+                  value={policyNumber}
+                  onChange={(e) => setPolicyNumber(e.target.value)}
+                  className={styles.form_control_custom}
+                />
+              </Form.Group>
+            </Col>
+            <Col xs={12} md="auto">
               <Form.Group controlId="formDateRange">
-                <InputGroup>
+                <Form.Label>날짜 범위 :</Form.Label>
+                <InputGroup className={styles.input_group_custom}>
                   <Form.Control
                     type="date"
                     value={startDate}
@@ -129,8 +150,8 @@ const Long = () => {
                 </InputGroup>
               </Form.Group>
             </Col>
-            <Col>
-              <Button onClick={fetchData}>검색</Button>
+            <Col xs={12} md="auto">
+              <Button onClick={fetchData} className={styles.button_custom}>검색</Button>
             </Col>
           </Row>
         </Form>
@@ -141,50 +162,52 @@ const Long = () => {
             </Spinner>
           </div>
         ) : (
-          <Table striped bordered hover className={styles.table_custom}>
-            <thead>
-              <tr>
-                <th className={styles.contract_date}>{dateType === 'contractDate' ? '계약일' : dateType === 'paymentEndDate' ? '만기일' : '개시일'}</th>
-                <th className={styles.insurance_company}>보험회사</th>
-                <th>보험상품</th>
-                <th className={styles.birth_gender}>생년월일 / 성별</th>
-                <th className={styles.name}>피보험자</th>
-                <th className={styles.name}>계약자</th>
-                <th>증권번호</th>
-                <th>지점</th>
-                <th>팀</th>
-                <th>담당</th>
-                <th>납입보</th>
-                <th>수정보</th>
-                <th className={styles.name}>납입</th>
-                <th className={styles.name}>상태</th>
-                <th>회차</th>
-                <th className={styles.name}>업무</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item, index) => (
-                <tr key={index}>
-                  <td>{dateType === 'contractDate' ? item.contractDate : dateType === 'paymentEndDate' ? item.paymentEndDate : item.paymentStartDate}</td>
-                  <td>{item.contractCompany}</td>
-                  <td>{item.longTermProduct}</td>
-                  <td>{item.birthdate_gender}</td>
-                  <td>{item.insuredPerson}</td>
-                  <td>{item.contractor}</td>
-                  <td>{item.policyNumber}</td>
-                  <td>{item.branch}</td>
-                  <td>{item.team}</td>
-                  <td>{item.responsible}</td>
-                  <td>{formatNumber(item.paymentInsurance)}</td>
-                  <td>{formatNumber(item.correctedInsurance)}</td>
-                  <td>{item.paymentMethod}</td>
-                  <td>{item.contractStatus}</td>
-                  <td>{formatTerm(item.totalTerm)}</td>
-                  <td>{item.counselor}</td>
+          <div className="table-responsive">
+            <Table striped bordered hover className={styles.table_custom}>
+              <thead>
+                <tr>
+                  <th className={styles.contract_date}>{dateType === 'contractDate' ? '계약일' : dateType === 'paymentEndDate' ? '만기일' : '개시일'}</th>
+                  <th className={styles.insurance_company}>보험회사</th>
+                  <th>보험상품</th>
+                  <th className={styles.birth_gender}>생년월일 / 성별</th>
+                  <th className={styles.name}>피보험자</th>
+                  <th className={styles.name}>계약자</th>
+                  <th>증권번호</th>
+                  <th>지점</th>
+                  <th>팀</th>
+                  <th>담당</th>
+                  <th>납입보</th>
+                  <th>수정보</th>
+                  <th className={styles.name}>납입</th>
+                  <th className={styles.name}>상태</th>
+                  <th>회차</th>
+                  <th className={styles.name}>업무</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody>
+                {data.map((item, index) => (
+                  <tr key={index}>
+                    <td>{dateType === 'contractDate' ? item.contractDate : dateType === 'paymentEndDate' ? item.paymentEndDate : item.paymentStartDate}</td>
+                    <td>{item.contractCompany}</td>
+                    <td>{item.longTermProduct}</td>
+                    <td>{item.birthdate_gender}</td>
+                    <td>{item.insuredPerson}</td>
+                    <td>{item.contractor}</td>
+                    <td>{item.policyNumber}</td>
+                    <td>{item.branch}</td>
+                    <td>{item.team}</td>
+                    <td>{item.responsible}</td>
+                    <td>{formatNumber(item.paymentInsurance)}</td>
+                    <td>{formatNumber(item.correctedInsurance)}</td>
+                    <td>{item.paymentMethod}</td>
+                    <td>{item.contractStatus}</td>
+                    <td>{formatTerm(item.totalTerm)}</td>
+                    <td>{item.counselor}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
         )}
       </Container>
     </div>
