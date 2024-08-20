b8dd5a9cd56a5b7bb0d779cd1cfddc1d9b04ccfc
heondolee <heondo999@inu.ac.kr>
2024-08-20 22:09:10 +0900

    다운완료 알림

--- src/components/Long/DownloadBtn.js
+++ src/components/Long/DownloadBtn.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 function DownloadButton({ modelName }) {
+  const [isDownloading, setIsDownloading] = useState(false);
+
   const handleDownload = async () => {
     try {
+      setIsDownloading(true);
+
       const response = await axios({
         url: `${API_URL}/download-excel`,
         method: 'POST',
@@ -26,14 +30,21 @@ function DownloadButton({ modelName }) {
       link.click();
       link.parentNode.removeChild(link);
 
+      alert('다운이 완료됐습니다!');  // 다운로드 완료 후 알림
+
     } catch (error) {
       console.error('Download failed', error);
+      alert('다운로드 실패');
+    } finally {
+      setIsDownloading(false);
     }
   };
 
   return (
     <div>
-      <Button onClick={handleDownload}>{modelName} 엑셀 다운</Button>
+      <Button onClick={handleDownload} disabled={isDownloading}>
+        {modelName} 엑셀 다운
+      </Button>
     </div>
   );
 }
